76ea0500a2f730da306b8a8881f3bac9def24cf9
Mahadi Hasan Fardin <117199009+FardinMahadi@users.noreply.github.com>
2022-12-28 10:11:44 +0600

    Add files via upload

---
+++ _10lowercase2uppercase.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    char lowerchar, upperchar;
+    int ascii;
+    
+    //ASCII values of A-Z (uppercase alphabets) are 65-90
+    //And ASCII values of a-z (lowercase alphabets) are 97-122
+
+    printf("\nEnter a character: ");
+    scanf("%c", &lowerchar);
+
+    ascii= lowerchar;
+    upperchar=ascii-32;
+
+    printf("\nThe uppercase is: %c", upperchar);
+    
+    return 0;
+}
